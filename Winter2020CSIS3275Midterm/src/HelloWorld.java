44e60ed176d84d7660b290305a21b103fe58e32e
Jaspreet86971 <kaurj62@student.douglascollege.ca>
2020-02-14 13:36:47 -0800

    This is new commit message[Feb 14,2020].

---
+++ Winter2020CSIS3275Midterm/src/HelloWorld.java
@@ -0,0 +1,12 @@
+
+public class HelloWorld {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		
+		System.out.println("Jaspreet");
+		System.out.println("kaurj62@student.douglascollege.ca");
+
+	}
+
+}
